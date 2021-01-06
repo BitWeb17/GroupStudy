94bb8a996d70699d036bf8c0d8867686ec2cef7e
bitweb <bumttang01@naver.com>
2021-01-06 15:32:39 +0900

    Java & Git Test - Bumjin Lee

--- 3/bumjinlee/java_work/src/Sixth/GitTest.java
+++ 3/bumjinlee/java_work/src/Sixth/GitTest.java
@@ -2,6 +2,6 @@ package Sixth;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Java & Git Test on Ubuntu 18.04");
+        System.out.println("Java & Git Test on Ubuntu 18.04!");
     }
 }
