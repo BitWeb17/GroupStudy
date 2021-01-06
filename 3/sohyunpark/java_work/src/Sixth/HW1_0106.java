9aa630b3d62aad35dcace63226d22c07e3b56b03
PARK-AA <passion414@naver.com>
2021-01-06 22:54:22 +0900

    2021-01-06 (7일차) 강의 + 과제

---
+++ 3/sohyunpark/java_work/src/Sixth/HW1_0106.java
@@ -0,0 +1,13 @@
+package Sixth;
+
+public class HW1_0106 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[7];
+
+        for (int i = 0; i < 7; i++) {
+            arr[i] = i + 1;
+            System.out.printf("arr[%d] = %d\n", i, arr[i]);
+        }
+    }
+}
