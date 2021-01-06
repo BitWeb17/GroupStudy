685e93ea7da0de2c6b483c84ed60af692128b684
cksdnr3 <cksdnr3@naver.com>
2021-01-06 21:33:20 +0900

    Sixth Homework

--- 3/ChanUkNoh/java_work/src/Sixth/HomeWork1.java
+++ 3/ChanUkNoh/java_work/src/Sixth/HomeWork1.java
@@ -2,17 +2,6 @@ package Sixth;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-
-        // 0) 팀원들 확인하기기
-
-
-
-
-
-
-        // 제출은 git을 통해 PR(Pull Request)까지 넣기!
-
-
         // 1) 7개 짜리 int 형 배열을 만들어보자!
 
         int[] sevenArr = new int[7];
