e803f02dfd72bcbf3ed261b9acb4371da78b66e5
HaJinJu <flfkwlswn7@naver.com>
2021-01-07 18:46:18 +0900

    Lecture

--- 4/HaJinJu/java_work/src/Homework/Homework1.java
+++ 4/HaJinJu/java_work/src/Homework/Homework1.java
@@ -1,14 +1,10 @@
 package Homework;
 
 public class Homework1 {
-    public static void main(String[] args) {
-
-    // 1) 7개 짜리 int 형 배열을 만들어보자!
-    int [] arr = new int[7];
-
-     for(int i = 0; i < 7; i++){
-            arr[i] = i + 1;
-            System.out.printf("arr[%d] = %d\n", i, arr[i]);
+        public static void main(String[] args) {
+            final int ARRSIZE = 7;
+            // 1) 7개 짜리 int 형 배열을 만들어보자!
+            // int[] arr = new int[7];
+            int[] arr = new int[ARRSIZE];
         }
     }
-}
