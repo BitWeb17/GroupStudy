6527d4dec7d088e1b8da9f88af1c86743151e3f4
may-hj <hyeonjeong9943@gmail.com>
2021-01-08 17:27:56 +0900

    Eighth_ LectureContents_2021_01_08

---
+++ 2/HyeonJeong/java_work/src/Eighth_2021_01_08/HW_Answer05.java
@@ -0,0 +1,9 @@
+package Eighth_2021_01_08;
+
+public class HW_Answer05 {
+    public static void main(String[] args) {
+        // 5) 3 ~ 33 까지의 숫자중 2 의 배수 또는 3 의 배수인 수를 출력해보자!
+        MySerires ms = new MySerires();
+        ms.printTwiceOrder(3,33,2,3,ms.OR);
+    }
+}
---
+++ 2/HyeonJeong/java_work/src/Eighth_2021_01_08/HW_Answer06.java
@@ -0,0 +1,8 @@
+package Eighth_2021_01_08;
+
+public class HW_Answer06 {
+    public static void main(String[] args) {
+        MySerires ms = new MySerires();
+        ms.fibonacciSeries(15);
+    }
+}
