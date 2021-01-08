ade48c2adcdaec4b93b420d9efff851e3abdcd20
djd4532 <djd453216@nate.com>
2021-01-08 13:40:10 +0900

    Seventh Homework

---
+++ 1/ParkJaemin/java_work/src/Seventh/SeventhHomework/Number.java
@@ -0,0 +1,27 @@
+package Seventh.SeventhHomework;
+
+class Number {
+    int num;
+    int sum;
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+
+    public int getSum() {
+        return sum;
+    }
+}
+
+
+
