b2b3c151c3ee160d96c577ed8622fcb5b9e39d78
kibum414 <kibum414@naver.com>
2021-01-10 21:13:22 +0900

    9일 차 강의

--- 4/kibeompark/java_work/src/Eighth/Exercise3.java
+++ 4/kibeompark/java_work/src/Eighth/Exercise3.java
@@ -160,7 +160,7 @@ public class Exercise3 {
     public static void main(String[] args) {
         // 3) 학급에 학생이 20명 있다.
         //    각 학생의 성적을 랜덤으로 기록해 준다. (입력으로 받으면 힘드니까)
-        //    전체 학생들의 평균을 구하고 표춘편차와 분산을 구해 보자.
+        //    전체 학생들의 평균을 구하고 표준편차와 분산을 구해 보자.
         //    (분산과 표준편차 공식을 잘 모른다면 패스해도 OK)
         ScoreCalculator sc = new ScoreCalculator(20);
 
