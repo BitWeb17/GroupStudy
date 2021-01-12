5cc80a0926a4dd3e95e548ff8a03e19ac968b690
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-01-12 09:25:22 +0900

    2020_01_11 LectureContents_Jinwook

--- 2/Jinwook/java_work/src/Eighth/HomeworkReview/Questions_homework_eight_part.java
+++ 2/Jinwook/java_work/src/Eighth/HomeworkReview/Questions_homework_eight_part.java
@@ -5,6 +5,19 @@ public class Questions_homework_eight_part {
         // RockPaperScissors class에서 Scanner 부분에서 입력 부분에서
         // int 값을 주지 않고 String 값을 주어서 비교하여 승리 판독이 가능한가요?
         // ex) 1=가위,2=바위,3=보 ------> 가위,바위,보
+        // EX) Scanner scan = new Scanner(System.in);
+//             player = scan.nextLine();
+//             System.out.println();
+//             if (player == "가위") {
+//               System.out.println(scissors);
+//                }
+//            if (player == "주먹") {
+//              System.out.println(rock);
+//             }
+//            if (player == "보") {
+//            System.out.println(paper);
+//        }
+//    }
 
         // WorkRate class에서 requiredMinute 구하는 식 (56번째 줄) 구조 한번더 설명 부탁드리겠습니다!요
 
