ca645f453ae8cf309871ad500065854ef84ccf47
may-hj <hyeonjeong9943@gmail.com>
2021-01-13 08:19:43 +0900

    Modify Content

--- 2/HyeonJeong/java_work/src/Ninth_2021_01_11/HW11_Answer.java
+++
@@ -1,9 +0,0 @@
-package Ninth_2021_01_11;
-
-public class HW11_Answer {
-    public static void main(String[] args) {
-        AllocRandom ar = new AllocRandom();
-
-
-    }
-}
--- 2/HyeonJeong/java_work/src/Ninth_2021_01_11/ScoreCalculator.java
+++
@@ -1,5 +0,0 @@
-package Ninth_2021_01_11;
-
-public class ScoreCalculator {
-
-}
