d155c2595e8d9c9c4b7163e3bccb517bb066459c
cksdnr3 <cksdnr3@naver.com>
2021-01-14 22:12:39 +0900

    2021 - 01 - 14 Twelfth Lecture

---
+++ 3/ChanUkNoh/java_work/src/Twelfth/Utility.java
@@ -0,0 +1,10 @@
+package Twelfth;
+
+public class Utility {
+    public static final int MYSTRCMP = 1;
+    public static final int STRCMPTEST = 2;
+
+    public static final int WORRIOR = 1;
+    public static final int THIEF = 2;
+    public static final int MAGICIAN = 3;
+}
