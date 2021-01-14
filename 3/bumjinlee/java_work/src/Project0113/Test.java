71fdd8713ab6c2a06736f7d71aef4703f4d07b92
bitweb <bumttang01@naver.com>
2021-01-14 20:48:48 +0900

    죄송합니다 테스트입니다..

--- 3/bumjinlee/java_work/src/Project0113/Test.java
+++ 3/bumjinlee/java_work/src/Project0113/Test.java
@@ -2,6 +2,6 @@ package Project0113;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("죄송합니다 테스트입니다.");
+        System.out.println("죄송합니다 테스트입니다..");
     }
 }
