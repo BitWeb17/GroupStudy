52b0ca0198781bd35eeca59b3bb878a4fca9447d
legossol <wkdgothf@gmail.com>
2021-01-17 23:06:50 +0900

    2021-01-13 13회차 과제 - 조짜기(fail)

---
+++ 1/janghaesol/HomeWork/src/jan0115/ProjectTeamUtility.java
@@ -0,0 +1,37 @@
+package jan0115;
+
+public class ProjectTeamUtility {
+
+
+}
+    /*
+        Object uiuxpeople;
+        Object uiuxteam;
+
+        private int[] uiuxpeopleArray;
+        private int[] uiuxteamArray;
+
+//        private ArrayList<String> uiuxpeopleArrayList;
+//        private ArrayList<String> uiuxteamArray;
+
+        // 생성자
+        public ProjectTeamUtility(int uiuxpeople, int uiuxteam){
+            this.uiuxpeople = uiuxpeople;
+            this.uiuxteam = uiuxteam;
+
+            uiuxpeopleArray = new int[17];
+            uiuxteamArray = new int[4];
+        }
+
+        public void setNumforAllmember(){
+            int personNum;
+
+            for(int i = 0; i < uiuxteam; i ++)
+
+        }
+*/
+
+
+
+
+
