00d223ee9d43f1b27d3d09ae4c9c528b040dcb9c
legossol <wkdgothf@gmail.com>
2021-01-18 17:53:46 +0900

    2021-01-18 14회차 강의내용 업데이트(장해솔)

--- janghaesol/java_work/src/Fourteenth/SocketClient.java
+++ janghaesol/java_work/src/Fourteenth/SocketClient.java
@@ -1,7 +1,10 @@
 package Fourteenth;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class SocketClient {
     private Socket mSocket;
@@ -9,18 +12,28 @@ public class SocketClient {
     private BufferedReader mIn;
     private PrintWriter mOut;
 
-    public SocketClient(Sttring ip, int portt){
+    public SocketClient(String ip, int port) {
         try {
             mSocket = new Socket(ip, port);
-            System.out.println(ip + "연결됨");
+            System.out.println(ip + " 연결됨");
 
             mIn = new BufferedReader(
-                    new InputStreamReader(mSocket.getInputStream())
+                    new InputStreamReader(
+                            mSocket.getInputStream())
             );
-            mOut = new PrintWritter(mSocket.getOutputStream());
-        }catch (IOEexception e ){
-            System.out.println(e.gettMessage());
-            )
+            mOut = new PrintWriter(mSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    public void closeSocket() {
+        try {
+            mSocket.close();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("연결 해제 성공!");
     }
 }
