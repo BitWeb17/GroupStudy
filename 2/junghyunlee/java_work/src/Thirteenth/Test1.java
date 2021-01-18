540f362865431461d087bd7b3bf3bc371e888a2a
akdl911215 <akdl911215@naver.com>
2021-01-18 09:21:08 +0900

    13일차 과제 입니다! - 이정현

---
+++ 2/junghyunlee/java_work/src/Thirteenth/Test1.java
@@ -0,0 +1,17 @@
+package Thirteenth;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int []checkDup = new int[17];
+
+
+            while(i < 17){
+                checkDup[i] = 0;
+                System.out.println(checkDup[i]);
+            }
+
+
+    }
+}
