af9049c2d24e99a8e37dcef238c036c86199114f
cksdnr3 <cksdnr3@naver.com>
2021-01-19 10:30:20 +0900

    2021 / 01 / 18 Homework (modified)

--- ChanUkNoh/java_work/src/Fourteenth/CreateTeam.java
+++ ChanUkNoh/java_work/src/Fourteenth/CreateTeam.java
@@ -62,13 +62,13 @@ public class CreateTeam {
         Iterator e = al.iterator();
 
         int cnt = 1;
-        int zeroCnt = 1;
+        int zeroCnt = 0;
 
         // numOfTeam 개의 팀에 quot 명을 팀으로 만듬
         int quot = al.size() / numOfTeam;
         // 총 인원과 만드려는 팀의 갯수가 맞지 않는 경우를 판별하기 위한 변수
         // quot 의 나머지
-        int remain = al.size() % numOfTeam;
+        int remain = al.size() % numOfTeam + 1;
         System.out.println(remain);
 
         // 예외처리가 필요한 경우 true 를 반환
@@ -89,19 +89,22 @@ public class CreateTeam {
             // 예외를 적용하지 않는 팀에게는 randValue 에 0을 적용해
             // 예외 팀이 한 번 더 출력되도록 한다.
 
+            // 전략: 예외 케이스의 경우를 찾아 예외 케이스를 적용해야한다.
+            //      remain 만큼의 예외가 필요하고 remain 이 모두 소모되면
+            //      예외케이스를 적용하지 않는다. (초기화는 비예외케이스로 초기화하고 예외케이스에만 적용)
+            //      하지만 randValue 가 모두 0이나오는 경우도 있기 때문에 처리해야함
+            //      zeroCnt 를 만들어 비예외 케이스가 필요한 경우의 수(numOfTeam - remain) 보다 크면
+            //      예외를 적용한다.
             if (remain > 0) {
                 if (needException) {
                     randValue = (int) (Math.random() * 2);
-
-                    if (remain < zeroCnt) randValue = 1;
-
                     if (randValue == 1) remain--;
+                    if (zeroCnt >= numOfTeam - remain) randValue = 1;
+                    if (randValue == 0) zeroCnt++;
 
                     needException = false;
                 }
             }
-
-            if (randValue == 0) zeroCnt++;
 
             // cnt 가 한 반 인원수 + 예외 인원수 만큼 증가하면 줄 바꿈
             if ((cnt % (quot + randValue)) == 0) {
