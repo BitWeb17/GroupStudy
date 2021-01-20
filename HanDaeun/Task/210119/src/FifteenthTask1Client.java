00c912d97b0df169ffd4f09c9dd4de9948556799
RayJUNRein <zkalkal27@gmail.com>
2021-01-20 11:03:11 +0900

    210119 Task Update (수정) - 한다은

--- HanDaeun/Task/210119/src/FifteenthTask1Client.java
+++ HanDaeun/Task/210119/src/FifteenthTask1Client.java
@@ -10,9 +10,9 @@ public class FifteenthTask1Client {
         String sendMsg, receivedMsg;
         Scanner scan = new Scanner(System.in);
 
+        // 서버의 정보를 입력받아 접속함
         System.out.print("접속하려는 서버의 IP 주소 : ");
         hostname = scan.nextLine();
-
         System.out.print("사용하려는 서비스의 포트 번호 : ");
         port = Integer.parseInt(scan.nextLine());
 
@@ -27,13 +27,15 @@ public class FifteenthTask1Client {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             RockPaperScissors rps = new RockPaperScissors();
+            System.out.println("\n당신은 Player2 입니다.");
 
 
             while(true) {
                 receivedMsg = reader.readLine();
                 System.out.println(receivedMsg);
 
-                if(receivedMsg.equals(rps.getSTART())) {
+                // 클라이언트의 입력이 필요할 때만 입력값을 서버에 넘겨줌
+                if(receivedMsg.equals(rps.getINPUT())) {
                     sendMsg = scan.nextLine();
                     writer.println(sendMsg);
                 }
--- HanDaeun/Task/210119/src/FifteenthTask1Server.java
+++ HanDaeun/Task/210119/src/FifteenthTask1Server.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class FifteenthTask1Server {
     public static void main(String[] args) {
         int port = Integer.parseInt("33333");
-        String[] receivedMsg = new String[2];
-        String sendMsg;
+        String receivedMsg;
+        String sendMsg, rpsInput;
 
         try {
+            System.out.println("Player 1 이 다른 플레이어를 기다리는 중입니다...\n");
+
             ServerSocket servSock = new ServerSocket(port);
             Socket clntSock = servSock.accept();
 
@@ -19,29 +22,41 @@ public class FifteenthTask1Server {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             RockPaperScissors rps = new RockPaperScissors();
+            Scanner scan = new Scanner(System.in);
 
-            System.out.println("[" + clntSock.getInetAddress() + "] 플레이어 입장하셨습니다.");
+            System.out.println("[" + clntSock.getInetAddress() + "] Player 2 가 입장하셨습니다.");
 
 
             while(true) {
+                // 새로운 게임을 세팅
                 sendMsg = "";
                 writer.println(sendMsg);
                 System.out.println(sendMsg);
-
-                rps.setRpsStart();
+                rps.setRpsInput();
                 sendMsg = rps.getMsg();
 
+                // Player 1(Server) 의 입력을 받음
                 System.out.println(sendMsg);
+                rps.setP2Wait();
+                sendMsg = rps.getMsg();
                 writer.println(sendMsg);
-                receivedMsg[0] = reader.readLine();
-                System.out.println("Player 1: " + receivedMsg[0]);
+                rpsInput = scan.nextLine();
 
-                System.out.println(sendMsg);
+                // Player 2(Client) 의 입력을 받음
+                rps.setRpsInput();
+                sendMsg = rps.getMsg();
                 writer.println(sendMsg);
-                receivedMsg[1] = reader.readLine();
-                System.out.println("Player 2: " + receivedMsg[1]);
+                System.out.println(rps.getP1WAIT());
+                receivedMsg = reader.readLine();
 
-                rps.rpsPlay(receivedMsg[0], receivedMsg[1]);
+                // 플레이어들이 입력한 것을 양쪽에 모두 출력해줌
+                System.out.println("Player 1: " + rpsInput);
+                System.out.println("Player 2: " + receivedMsg);
+                writer.println("Player 1: " + rpsInput);
+                writer.println("Player 2: " + receivedMsg);
+
+                // 게임의 결과를 양쪽에 모두 출력해줌
+                rps.rpsPlay(rpsInput, receivedMsg);
                 sendMsg = rps.getMsg();
                 System.out.println(sendMsg);
                 writer.println(sendMsg);
