2890aff175ea030e10071ff5dcbed2c71f4005b0
legossol <wkdgothf@gmail.com>
2021-01-22 00:06:20 +0900

    2021-01-21 평가 과제 진행중;

--- janghaesol/HomeWork/src/FirstRegularTest/Eleventh.java
+++ janghaesol/HomeWork/src/FirstRegularTest/Eleventh.java
@@ -4,39 +4,53 @@ class doubArr{
     int[][] arr2;
     int maxSize;
     int eachNum;
-    int sumNum;
 
 
     public void firstArr(){
         eachNum = (int)(Math.random()*10);
         maxSize = 2;
-        for(int i =0; i < maxSize; i ++){
-            for(int j = 0; j < maxSize; j ++){
-                System.out.printf("%2d",arr1[i][j]);
-            }
+       arr1 = new int[2][2];
+       for(int i = 0 ; i < arr1.length; i ++){
+           for(int j = 0; j < arr1[i].length;j++){
+               arr1[i][j] = eachNum;
+               eachNum += 1;
+               System.out.printf("%3d",arr1[i][j]);
+           }
+           System.out.println("");
         }
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
-                arr1[i][j] = eachNum;
-                System.out.print(arr1[i][j] + " ");
-            }
-            System.out.println();
-        }
-
     }
 
+    public void secondArr(){
+        eachNum = (int)(Math.random()*10);
+        maxSize = 2;
+        arr2 = new int [2][2];
+        for(int i = 0 ; i < arr2.length; i ++){
+            for(int j = 0; j < arr2[i].length;j++){
+                arr2[i][j] = eachNum;
+                eachNum += 2;
+                System.out.printf("%3d",arr2[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public void sumArr(int[][] arr1, int[][] arr2) {
+        int[][] sumFB = new int[arr1.length][arr1[0].length];
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr1[0].length; j++) {
+                sumFB[i][j] = arr1[i][j] + arr2[i][j];
+                System.out.printf("%5d",sumFB[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
 
 public class Eleventh {
     public static void main(String[] args) {
         doubArr dA = new doubArr();
-        int arr[][] = {
-                {1, 0},
-                {1, 0}
-        };
-
-
         dA.firstArr();
-
+        dA.secondArr();
+        dA.sumArr(dA.arr1, dA.arr2);
     }
 }
