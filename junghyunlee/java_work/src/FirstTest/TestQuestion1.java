2ed70fe826b20621e3ad211a391b3a51aa7f8b69
akdl911215 <akdl911215@naver.com>
2021-01-22 20:54:48 +0900

    17th 수업내용및 평가문제입니 - 이정현

---
+++ junghyunlee/java_work/src/FirstTest/TestQuestion1.java
@@ -0,0 +1,8 @@
+package FirstTest;
+
+public class TestQuestion1 {
+    public static void main(String[] args) {
+        inputScanner is = new inputScanner();
+        is.printScanner();
+    }
+}
---
+++ junghyunlee/java_work/src/FirstTest/TestQuestion2.java
@@ -0,0 +1,10 @@
+package FirstTest;
+
+public class TestQuestion2 {
+    public static void main(String[] args) {
+        inputScanner as = new inputScanner();
+
+        as.printScanner(5, 5);
+        as.NumberAverage();
+    }
+}
