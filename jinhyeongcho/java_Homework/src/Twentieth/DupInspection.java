03c5f34cd8517d2979b1a293935aa0e481c12f2e
korjjh1216 <korjjh1216@naver.com>
2021-01-27 00:21:43 +0900

    2021-01-26 HomeWork

--- jinhyeongcho/java_Homework/src/Twentieth/DupInspection.java
+++ jinhyeongcho/java_Homework/src/Twentieth/DupInspection.java
@@ -1,6 +1,7 @@
 package Twentieth;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -9,7 +10,6 @@ public class DupInspection {
     int [] randNum;
     int i;
     ArrayList<Integer> str = new ArrayList<>();
-    HashSet<Integer> hs = new HashSet<>();
 
 
 
@@ -17,39 +17,19 @@ public class DupInspection {
         randNum =new int[setNum];
         for (i =0 ;i< randNum.length;i++){
             randNum[i]=(int)(Math.random()*41)+20;
-            System.out.printf("%d번 배열 수:%d\n",i,randNum[i]);
+            //System.out.printf("%d번 배열 수:%d\n",i,randNum[i]);
             str.add(randNum[i]);
-
         }
+        for (int lists : str) ;
+        System.out.print(str);
     }
-
-
-    public void hashNum(){
-        for (Integer dup : str){
-            hs.add(dup);
-        }
-        System.out.println("이게 중복된 값= "+hs);
-    }
-
     public void hashDup(){
-      /*  int totalNum=0;
-        for (i = 0;i< randNum.length;i++){
-
-            for (int j=20; j<61;j++){
-                if(randNum[i]==j){
-
-                 totalNum++;
-
-                }
-                System.out.printf("%d값 중복된 수 = %d\n",j,totalNum);
-            }
-        }*/
-
-
-        Iterator e = str.iterator();
-        Iterator s = hs.iterator();
-        System.out.println(e.equals(s));
-
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        //hashmap 중 getorDefault(key, Default-value)메서드인데 뜻이 찾는 키가 존재한다면
+        //찾는 키의 값을 반환하고 없다면 기본 값을 반환하다는 뜻
+        //Default 값 반환하고 +1씩 해서 카운트.
+        for (int list2 : str) hm.put(list2, hm.getOrDefault(list2, 0) + 1);
+        System.out.printf("결과 : " + hm);
     }
 
 }
