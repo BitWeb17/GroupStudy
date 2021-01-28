cb0bd4e676e98add627571ea7389b85d019b7072
may-hj <hyeonjeong9943@gmail.com>
2021-01-29 01:27:35 +0900

    2021-01-28-homework unfinished

--- JAVASCRIPT/HyeonJeong/JavaScript_Work/src/JS_2021_01_28_Data_Type/DataType.js
+++ JAVASCRIPT/HyeonJeong/JavaScript_Work/src/JS_2021_01_28_Data_Type/DataType.js
@@ -1,6 +1,9 @@
 function DataType() {
+    // 숫자 + 문자열은 숫자를 강제로 문자열로 바꿈
     var test1 = 7 + "Test"
-    var test2 = "7" + "Test"
+    var test2 = "7" + "test"
+    // 숫자 + 숫자는 숫자로 계산하고
+    // 이후에 숫자 + 문자열로 인해 숫자를 문자열로 바꿈
     var numTest = 3 + 7 + "Test"
 
     console.log("DataType: " + test1)
@@ -8,9 +11,9 @@ function DataType() {
     console.log("DataType: " + numTest)
 
     return (
-        <div className = "DataType">
+        <div className="DataType">
             <p>
-                {test1},{test2},{numTest}.
+                {test1}, {test2}, {numTest}.
             </p>
         </div>
     )
