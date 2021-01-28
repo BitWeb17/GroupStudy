ebc351f3585e9f3ad018947cc84ec878cb449959
akdl911215 <akdl911215@naver.com>
2021-01-28 17:37:20 +0900

    java

--- junghyunlee/java_work/src/Twentieth/RandomNumberArrlist.java
+++ junghyunlee/java_work/src/Twentieth/RandomNumberArrlist.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RandomNumberArrlist {
-    private static Integer[] totalNum;
-    private int cnt = 1;
 
     public void printRandomNum(){
         ArrayList<Integer> list = new <Integer>ArrayList();
@@ -14,17 +12,14 @@ public class RandomNumberArrlist {
 
             list.add((int) (Math.random() * 40) + 20);
 
-            if(cnt % 5 == 0) {
                 for(int totalNum : list);
-            System.out.println("");
-        }
+
         }
 
 
         System.out.println("리스트 값 = " + list);
         HashMap<Integer, Integer> map = new HashMap<>();
-        // cnt를 for 에다 쓸수 있다.
-        for (Integer a : list) map.put(a, map.getOrDefault(a, 0) + 1);
+            for (Integer a : list) map.put(a, map.getOrDefault(a, 0) + 1);
         System.out.println("출력 결과 : " + map);
     }
 }
