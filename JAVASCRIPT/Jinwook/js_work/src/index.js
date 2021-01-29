01bee1c51b5c634cc7d04e17fe0f751025c3a524
jinwook Oh <jinwookoh@jinwookui-MacBookPro.local>
2021-01-29 16:49:09 +0700

    01.29 LectureContents Jinwook

---
+++ JAVASCRIPT/Jinwook/js_work/src/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import SpreadTest from './arry/SpreadTest';
+import SpreadTest2 from './arry/SpreadTest2';
+import SpreadTest3 from './arry/SpreadTest3';
+import SpreadTest4 from './arry/SpreadTest4';
+import SpreadTest5 from './arry/SpreadTest5';
+import SpreadTest6 from './arry/SpreadTest6';
+import SpreadTest7 from './arry/SpreadTest7';
+import CICDTest2 from './coincidence/CICDTest2';
+import CICDTest3 from './coincidence/CICDTest3';
+import CICDTest4 from './coincidence/CICDTest4';
+import CICDTest5 from './coincidence/CICDTest5';
+import CICDTest6 from './coincidence/CICDTest6';
+import FinalEqCicdTest from './coincidence/FinalEqCicdTest';
+import ArrayDataType from './data_type/ArrayDataType';
+import BoolDataType from './data_type/BoolDataType';
+import DataType from './data_type/DataType';
+import DataTypeBasic from './data_type/DataTypeBasic';
+import ExpDataType from './data_type/ExpDataType';
+import ForTest from './data_type/ForTest';
+import IfTest from './data_type/IfTest';
+import JsonDataType from './data_type/JsonDataType';
+import LetVar from './data_type/LetVar';
+import LetVar2 from './data_type/LetVar2';
+import LetVar3 from './data_type/LetVar3';
+// import LetVar4 from './data_type/LetVar4';
+import RealDataType from './data_type/RealDataType';
+import StringDataType from './data_type/StringDataType';
+import SymbolTest from './data_type/SymbolTest';
+import TypeOf from './data_type/TypeOf';
+import EQTest from './equals/EQTest';
+import EQTest2 from './equals/EQTest2';
+import EQTest3 from './equals/EQTest3';
+import EQTest4 from './equals/EQTest4';
+import EQTest5 from './equals/EQTest5';
+import EQTest6 from './equals/EQTest6';
+import EmptyObjectTest from './function/EmptyObjectTest';
+import FunctionObjectTest from './function/FunctionObjectTest';
+import FunctionObjectTest2 from './function/FunctionObjectTest2';
+import Homework1 from './homework/Homework1';
+import Homework2 from './homework/Homework2';
+import Homework3 from './homework/Homework3';
+import Homework4 from './homework/Homework4';
+import LiteralTest from './literal/LiteralTest';
+import RandomTeam from './quiz/RandomTeam';
+import UpgradedRandomTeam from './quiz/UpgradedRandomTeam';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SpreadTest/>
+    <SpreadTest2/>
+    <SpreadTest3/>
+    <SpreadTest4/>
+    <SpreadTest5/>
+    <SpreadTest6/>
+    <SpreadTest7/>
+    <CICDTest2/>
+    <CICDTest3/>
+    <CICDTest4/>
+    <CICDTest5/>
+    <CICDTest6/>
+    <FinalEqCicdTest/>
+    <ArrayDataType/>
+    <BoolDataType/>
+    <DataType/>
+    <DataTypeBasic/>
+    <ExpDataType/>
+    <ForTest/>
+    <IfTest/>
+    <JsonDataType/>
+    <LetVar/>
+    <LetVar2/>
+    <LetVar3/>
+    {/* <LetVar4/> */}
+    <RealDataType/>
+    <StringDataType/>
+    <SymbolTest/>
+    <TypeOf/>
+    <EQTest/>
+    <EQTest2/>
+    <EQTest3/>
+    <EQTest4/>
+    <EQTest5/>
+    <EQTest6/>
+    <EmptyObjectTest/>
+    <FunctionObjectTest/>
+    <FunctionObjectTest2/>
+    <Homework1/>
+    <Homework2/>
+    <Homework3/>
+    <Homework4/>
+    <LiteralTest/>
+    <RandomTeam/>
+    <UpgradedRandomTeam/>
+
+
+
+    </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
