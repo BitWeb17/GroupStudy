215fd049224ce5385caa43e77bc7b4d76e59046d
djd4532 <djd453216@nate.com>
2021-01-29 09:22:21 +0900

    Homework & ReviewNote

---
+++ JAVASCRIPT/ParkJaemin/js_test/src/data_type/Homework & ReviewNote/Q1.js
@@ -0,0 +1,20 @@
+function Q1(){
+
+    for(var i = 1; i<=10; i++){
+        console.log("1부터 10까지의 나열 : " + i)
+    }
+
+    return (
+        <div className="Q1">
+            <p>
+
+                OneTen Works Fine!
+
+            </p>
+
+        </div>
+    )
+
+}
+
+export default Q1()
