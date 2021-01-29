d8fe7529b198f36630bb83c1b852c852ff61a105
RYUWEULGI <rhythm296@naver.com>
2021-01-29 17:20:50 +0900

    21/01/29 TIL js 배열,함수

---
+++ JAVASCRIPT/RYUSEULGI/lecture/src/Test129/LiteralTest.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function LiteralTest() {
+
+    const first = 3;
+    const second = 7;
+
+    const str2 = `${first} * ${second} = ??`;
+
+    console.log(str2);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LiteralTest;
