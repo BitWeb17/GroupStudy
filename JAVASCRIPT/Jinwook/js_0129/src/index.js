a80da32ceaa4006e8dd2eebb97fb804352c8b63b
jinwook Oh <jinwookoh@jinwookui-MacBookPro.local>
2021-02-01 00:18:55 +0900

    Studies PDF

--- JAVASCRIPT/Jinwook/js_0129/src/index.js
+++ JAVASCRIPT/Jinwook/js_0129/src/index.js
@@ -3,10 +3,71 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SpreadTest2 from './200129works/arry/SpreadTest2';
+import SpreadTest3 from './200129works/arry/SpreadTest3';
+import SpreadTest4 from './200129works/arry/SpreadTest4';
+import SpreadTest5 from './200129works/arry/SpreadTest5';
+import SpreadTest6 from './200129works/arry/SpreadTest6';
+import SpreadTest7 from './200129works/arry/SpreadTest7';
+import SpreadTest from './200129works/arry/SpreadTest';
+import CICDTest2 from './coincidence/CICDTest2';
+import CICDTest3 from './coincidence/CICDTest3';
+import CICDTest4 from './coincidence/CICDTest4';
+import CICDTest5 from './coincidence/CICDTest5';
+import CICDTest6 from './coincidence/CICDTest6';
+import FinalEqCicdTest from './coincidence/FinalEqCicdTest';
+import EQTest from './equals/EQTest';
+import EQTest2 from './equals/EQTest2';
+import EQTest3 from './equals/EQTest3';
+import EQTest4 from './equals/EQTest4';
+import EQTest5 from './equals/EQTest5';
+import EQTest6 from './equals/EQTest6';
+import EmptyObjectTest from './function/EmptyObjectTest';
+import FunctionObjectTest from './function/FunctionObjectTest';
+import FunctionObjectTest2 from './function/FunctionObjectTest2';
+import Homework1 from './homework/Homework1';
+import Homework2 from './homework/Homework2';
+import Homework3 from './homework/Homework3';
+import Homework4 from './homework/Homework4';
+import LiteralTest from './literal/LiteralTest';
+import RandomTeam from './quiz/RandomTeam';
+import UpgradedRandomTeam from './quiz/UpgradedRandomTeam';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SpreadTest/>
+    <SpreadTest2/>
+    <SpreadTest3/>
+    <SpreadTest4/>
+    <SpreadTest5/>
+    <SpreadTest6/>
+    <SpreadTest7/>
+    <CICDTest2/>
+    <CICDTest3/>
+    <CICDTest4/>
+    <CICDTest5/>
+    <CICDTest6/>
+    <FinalEqCicdTest/>
+    <EQTest/>
+    <EQTest2/>
+    <EQTest3/>
+    <EQTest4/>
+    <EQTest5/>
+    <EQTest6/>
+    <EmptyObjectTest/>
+    <FunctionObjectTest/>
+    <FunctionObjectTest2/>
+    <Homework1/>
+    <Homework2/>
+    <Homework3/>
+    <Homework4/>
+    <LiteralTest/>
+    <RandomTeam/>
+    <UpgradedRandomTeam/>
+
+    
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
