c99c7f294b23cac8c26864ff9b1e10388ed7eca0
korjjh1216 <korjjh1216@naver.com>
2021-02-01 20:34:32 +0900

    2021-02-01 Js HomeWork

--- JAVASCRIPT/jinhyeongcho/js_test/src/index.js
+++ JAVASCRIPT/jinhyeongcho/js_test/src/index.js
@@ -2,44 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ObjectTest from "./objest-test/ObjectTest";
-import ObjectTest2 from "./objest-test/ObjectTest2";
-import ClassObject from "./class-test/ClassObject";
-import ClassObject2 from "./class-test/ClassObject2";
-import ClassObject3 from "./class-test/ClassObject3";
-import ClassObject4 from "./class-test/ClassObject4";
-import ClassExtends from "./class-extends-test/ClassExtends";
-import ClassExtends2 from "./class-extends-test/ClassExtends2";
-import ArrayJoinTest from "./array-test/ArrayJoinTest";
-import ArrayPopTest from "./array-test/ArrayPopTest";
-import ArrayPushTest from "./array-test/ArrayPushTest";
-import ArrayShiftTest from "./array-test/ArrayShiftTest";
-import ArrayUnshiftTest from "./array-test/ArrayUnshiftTest";
-import ArraySpliceTest from "./array-test/ArraySpliceTest";
-import ArrayConcatTest from "./array-test/ArrayConcatTest";
-import ArraySliceTest from "./array-test/ArraySliceTest";
-import SortTest from "./sort/SortTest";
+import Work1 from "./homeWorks/02-01/Work1";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ObjectTest/>
-    <ObjectTest2/>
-    <ClassObject/>
-    <ClassObject2/>
-    <ClassObject3/>
-    <ClassObject4/>
-    <ClassExtends/>
-    <ClassExtends2/>
-    <ArrayJoinTest/>
-    <ArrayPopTest/>
-    <ArrayPushTest/>
-    <ArrayShiftTest/>
-    <ArrayUnshiftTest/>
-    <ArraySpliceTest/>
-    <ArrayConcatTest/>
-    <ArraySliceTest/>
-    <SortTest/>
+    <Work1/>
   </React.StrictMode>,
   document.getElementById('root')
 );
