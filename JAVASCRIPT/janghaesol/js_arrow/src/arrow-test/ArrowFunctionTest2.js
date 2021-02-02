76561edbc1386ff9fa9521d536bd810969a8b59f
legossol <wkdgothf@gmail.com>
2021-02-02 19:25:02 +0900

    2021-02-02 강의내용 업데이트

---
+++ JAVASCRIPT/janghaesol/js_arrow/src/arrow-test/ArrowFunctionTest2.js
@@ -0,0 +1,19 @@
+//인자를 받는 방식의 ArrowFunction
+const ArrowFunctionTest2 = () =>{
+    let add333 = function(val) { return" 333" + val}
+    console.log("ArrowFunctionTest =" + add333(77))
+            
+            
+    let add333Arrow = (val) => {return"333"+ val}
+    console.log("ArrowFunctionTest =" + add333Arrow(77))
+
+    return (
+        <div className = "ArrowFunctionTest2">
+            <p>
+            ArrowFunctionTest2
+            </p>
+        </div>
+    )
+}
+
+export default ArrowFunctionTest2
