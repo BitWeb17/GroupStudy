21150e516de2d6239ecd6c8796ba138800b041e4
RYUSEULGI <rhythm296@naver.com>
2021-02-05 17:41:29 +0900

    21/02/05 숙제

---
+++ SPRING/RYUSEULGI/study/study210205/src/main/java/com/example/study210205/Study210205Application.java
@@ -0,0 +1,13 @@
+package com.example.study210205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study210205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study210205Application.class, args);
+	}
+
+}
