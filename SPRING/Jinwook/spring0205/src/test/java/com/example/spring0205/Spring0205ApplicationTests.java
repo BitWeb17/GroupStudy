cd2fba3ded3c311c8376f64461ec11ac817db16c
jinwook Oh <jinwookoh@jinwookui-MacBookPro.local>
2021-02-05 17:31:38 +0900

    LectureContents0205

---
+++ SPRING/Jinwook/spring0205/src/test/java/com/example/spring0205/Spring0205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring0205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0205ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
