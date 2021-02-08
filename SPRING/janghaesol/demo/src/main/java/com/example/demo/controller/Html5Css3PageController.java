ab182c8205d43a58f63e667b2b9dfb59b53a2d4c
legossol <wkdgothf@gmail.com>
2021-02-08 19:45:11 +0900

    2021-02-08강의내용 업데이트

--- SPRING/janghaesol/demo/src/main/java/com/example/demo/controller/Html5Css3PageController.java
+++ SPRING/janghaesol/demo/src/main/java/com/example/demo/controller/Html5Css3PageController.java
@@ -127,4 +127,116 @@ public class Html5Css3PageController {
 
         return "html_css/success.html";
     }
+
+    @GetMapping("/tableTest")
+    public String gettableTest() {
+        log.info("Enter tableTest()");
+
+        return "html_css/tableTest.html";
+    }
+
+    @GetMapping("/mat3by3Test")
+    public String getmat3by3Test(){
+        log.info("Enter mat3by3Test");
+
+        return "html_css/mat3by3Test.html";
+    }
+    
+    @GetMapping("/tableTest2")
+    public String gettableTest2() {
+        log.info("Enter tableTest2");
+
+        return "html_css/tableTest2.html";
+    }
+
+    @GetMapping("/captionTest")
+    public String getcaptionTest() {
+        log.info("Enter captionTest");
+
+        return "html_css/captionTest.html";
+    }
+    @GetMapping("/tableTest3")
+    public String gettableTest3() {
+        log.info("Enter tableTest3");
+
+        return "html_css/tableTest3.html";
+    }
+    @GetMapping("/tableTest4")
+    public String getTableTest4() {
+        log.info("Enter getTableTest4()");
+
+        return "html_css/tableTest4.html";
+    }
+
+    @GetMapping("/tableTest5")
+    public String getTableTest5() {
+        log.info("Enter getTableTest5()");
+
+        return "html_css/tableTest5.html";
+    }
+
+    @GetMapping("/imgAltTest")
+    public String getImgAltTest() {
+        log.info("Enter getImgAltTest()");
+
+        return "html_css/imgAltTest.html";
+    }
+
+    @GetMapping("/imgSizeTest")
+    public String getImgTest() {
+        log.info("Enter getImgSizeTest()");
+
+        return "html_css/imgSizeTest.html";
+    }
+
+    @GetMapping("/imgFigcaptionTest")
+    public String getFigcaptionTest() {
+        log.info("Enter getFigcaptionTest()");
+
+        return "html_css/imgFigcaptionTest.html";
+    }
+
+    @GetMapping("/imgCompanyProfileTest")
+    public String getImgCompanyProfileTest() {
+        log.info("Enter getImgCompanyProfileTest()");
+
+        return "html_css/imgCompanyProfileTest.html";
+    }
+
+    @GetMapping("/aTagTest")
+    public String getATagTest() {
+        log.info("Enter getATagTest()");
+
+        return "html_css/aTagTest.html";
+    }
+
+    @GetMapping("/aTagStyleTest")
+    public String getATagStyleTest() {
+        log.info("Enter getATagStyleTest()");
+
+        return "html_css/aTagStyleTest.html";
+    }
+
+    /* iframe 부분에 예상치 못한 오류가 발생중
+       스펙 사항이 바뀌었는지 체크할 필요가 있음 */
+    @GetMapping("/iFrameTest")
+    public String getIFrameTest() {
+        log.info("Enter getIFrameTest()");
+
+        return "html_css/iFrameTest.html";
+    }
+
+    @GetMapping("/idCheckTest")
+    public String getIdCheckTest() {
+        log.info("Enter getIdCheckTest()");
+
+        return "html_css/idCheckTest.html";
+    }
+
+    @GetMapping("/visitedTest")
+    public String getvisitedTest() {
+        log.info("Enter getvisitedTest()");
+
+        return "html_css/visitedTest.html";
+    }
 }
