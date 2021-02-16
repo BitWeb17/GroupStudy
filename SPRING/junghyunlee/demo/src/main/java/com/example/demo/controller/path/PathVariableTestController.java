cf0228ae8441b0d0eaf057a6397e951ab2a81fa2
45277239 <45277239@naver.com>
2021-02-16 18:19:49 +0900

    32th spring 수업내용입니다. -정

--- SPRING/junghyunlee/demo/src/main/java/com/example/demo/controller/path/PathVariableTestController.java
+++ SPRING/junghyunlee/demo/src/main/java/com/example/demo/controller/path/PathVariableTestController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller.path;
 
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,29 +15,29 @@ public class PathVariableTestController {
     public String getBoardRegister() {
         log.info("getBoardRegister(): 회원가입");
 
-        return "sping/board/register.html";
+        return "spring/board/register.html";
     }
 
     @GetMapping("/board/modify")
     public String getBoardModify() {
         log.info("getBoardModify(): 정보 수정");
 
-        return "sping/board/modify.html";
+        return "spring/board/modify.html";
     }
 
     @GetMapping("/board/list")
     public String getBoardList() {
-        log.info("getBoardList(): 정보 목");
+        log.info("getBoardList(): 정보 목록");
 
-        return "sping/board/list.html";
+        return "spring/board/list.html";
     }
 
     @GetMapping("/board/read/{boardNo}")
     public String getBoardReadBoardNo(
             @PathVariable("boardNo") int boardNo) {
-        log.info("getBoardReadBoardNo(): 게시물 번호별 읽기");
+        log.info("getBoardReadBoardNo(): " +
+                "게시물 번호별 읽기 = " + boardNo);
 
         return "spring/board/read.html";
     }
-    )
 }
