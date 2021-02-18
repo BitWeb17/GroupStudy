f6a3911daa09e1fc23ac85500392d7051ab9ee80
djd4532 <djd453216@nate.com>
2021-02-18 09:42:27 +0900

    210217 Homework

--- SPRING/ParkJaemin/demo/src/main/java/com/example/demo/entity/TestMember.java
+++ SPRING/ParkJaemin/demo/src/main/java/com/example/demo/entity/TestMember.java
@@ -12,6 +12,9 @@ import java.util.List;
 public class TestMember {
     private String userName="bitweb";
     private String password="456123";
+    private String passwordiden="456123";
+    private String phoneNum="010-78421-888987";
+    private String Email="djd453216@nate.com";
     private String introduction;
     private List<String> hobbyList;
 
