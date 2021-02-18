8c1b33f85fec2702201cb94c6b45c905769e02f8
may-hj <hyeonjeong9943@gmail.com>
2021-02-19 00:41:56 +0900

    2021-02-18 Lecture Contents

---
+++ SPRING/HyeonJeong/spring/src/main/java/com/example/demo/controller/board/BoardController.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller.board;
+
+import com.example.demo.entity.Board;
+import com.example.demo.service.BoardService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/real_board")
+public class BoardController {
+    private static final Logger log =
+            LoggerFactory.getLogger(BoardController.class);
+
+    @Autowired
+    private BoardService service;
+
+    @GetMapping("/list")
+    public String getlist(Model model) throws Exception {
+        log.info("getlist()");
+
+        model.addAttribute("list", service.list());
+
+        return "spring/real_board/list";
+    }
+
+    // HTML 처리시 board 정보를 가지고 처리하게됨(주의)
+    @GetMapping("/register")
+    public String getRegister(Board board) {
+        log.info("getRegister()");
+
+        return "spring/real_board/register";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(Board board, Model model)
+            throws Exception {
+
+        log.info("doRegister()");
+
+        service.register(board);
+
+        model.addAttribute("msg","등록이 완료되었습니다.");
+
+        return "spring/real_board/success";
+    }
+
+    @GetMapping("/read")
+    public String read(int boardNo, Model model)
+            throws Exception {
+
+        log.info("read()");
+
+        model.addAttribute(service.read(boardNo));
+
+        return "spring/real_board/read";
+    }
+
+}
